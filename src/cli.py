289f0c42686ac5ec30a2bab6b3071b265ec4a983
Thomas Tumiel <ttumiel@gmail.com>
2020-02-25 04:01:20 +0200

    update cli and args

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,11 @@
 import argparse
+import logging
 
 from slicer import generate_gcode
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+
 
 def cli():
     p = argparse.ArgumentParser(
@@ -18,7 +23,7 @@ def cli():
         "--layer_height",
         "-l",
         type=float,
-        default=1.0,
+        default=0.2,
         help="The height of the slices in mm",
     )
     p.add_argument(
@@ -33,15 +38,64 @@ def cli():
         action="store_true",
         help="Save an image of the final extrusion path to out.jpg",
     )
+    p.add_argument(
+        "--feedrate",
+        "-f",
+        type=float,
+        default=3600,
+        help="The speed at which the printer head moves in mm/min. (Default: 3600mm/min)",
+    )
+    p.add_argument(
+        "--feedrate_writing",
+        type=float,
+        default=None,
+        help="The speed at which the printer head moves while printing in mm/min. (Default: 0.5*feedrate)",
+    )
+    p.add_argument(
+        "--filament_diameter",
+        "-d",
+        type=float,
+        default=1.75,
+        help="The diameter of FDM filament you are using",
+    )
+    p.add_argument(
+        "--extrusion_width",
+        "-w",
+        type=float,
+        default=0.4,
+        help="Total width of the capsule shaped cross section of a squashed filament.",
+    )
+    p.add_argument(
+        "--extrusion_multiplier",
+        type=float,
+        default=1,
+        help="The length of extrusion filament to be pushed through on a move"
+        "command will be multiplied by this number before being applied.",
+    )
+    p.add_argument(
+        "--units",
+        type=str,
+        default="mm",
+        choices=["in", "mm"],
+        help="The units to operate in. Either mm or in.",
+    )
+
+    # TODO: wall_speed, infill_speed
 
     args = p.parse_args()
 
     generate_gcode(
         args.filename,
         outfile=args.output,
-        slice_height=args.slice_height,
+        layer_height=args.layer_height,
         scale=args.scale,
         save_image=args.save_image,
+        feedrate=args.feedrate,
+        feedrate_writing=args.feedrate_writing,
+        filament_diameter=args.filament_diameter,
+        extrusion_width=args.extrusion_width,
+        extrusion_multiplier=args.extrusion_multiplier,
+        units=args.units,
     )
 
 
