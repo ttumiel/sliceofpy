103628ff6727f414fe5c62e1ec062cafeaa05c2f
Thomas Tumiel <ttumiel@gmail.com>
2020-03-03 00:57:50 +0200

    fix slicer tests

--- tests/test_slicer.py
+++ tests/test_slicer.py
@@ -1,4 +1,7 @@
+import os
 from sliceofpy.slicer import generate_gcode
 
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
 def test_generate_gcode():
-    generate_gcode("block.obj")
+    generate_gcode(os.path.join(__location__, "./block.obj"))
