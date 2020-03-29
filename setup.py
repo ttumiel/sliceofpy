c984ab1b85dba6d52c48ce45f1dca7a03e943468
Thomas Tumiel <ttumiel@gmail.com>
2020-03-29 16:03:44 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ setup(
     install_requires=[
         'numpy',
         'matplotlib',
-        'mecode',
-        'mayavi'
+        'mecode'
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
