20a01291f0285b6cffc8f6059675bd8c4b95da24
Thomas Tumiel <ttumiel@gmail.com>
2020-04-09 14:46:14 +0200

    add additional objects for tests

--- tests/test_slicer.py
+++ tests/test_slicer.py
@@ -3,5 +3,20 @@ from sliceofpy.slicer import generate_gcode
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-def test_generate_gcode():
+def test_generate_gcode_block():
     generate_gcode(os.path.join(__location__, "./block.obj"))
+
+def test_generate_gcode_pyramid():
+    generate_gcode(os.path.join(__location__, "./pyramid.obj"))
+
+def test_generate_gcode_ring():
+    generate_gcode(os.path.join(__location__, "./ring.obj"))
+
+def test_generate_gcode_doubleblock():
+    generate_gcode(os.path.join(__location__, "./2block.obj"))
+
+def test_generate_gcode_icecream(): # mmm
+    generate_gcode(os.path.join(__location__, "./icecream.obj"))
+
+def test_generate_gcode_torus():
+    generate_gcode(os.path.join(__location__, "./torus.obj"))
